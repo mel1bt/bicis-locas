89475b5efcdbeb80f90a0b525cdf41a6f5a9cb4a
mel1bt <mberrocal.tipte@gmail.com>
2016-09-15 12:26:23 -0500

    Actualizando parte 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nom = document.getElementById("name").value;
+	var ape = document.getElementById("lastname").value;
+	var cor = document.getElementById("input-email").value;
+	var contr = document.getElementById("input-password").value;
+	//var opcion = document.getElementById("checkbox").selectedIndex;
+
+    if(nom == null || nom.length == 0 || /^\s+$/.test(nom) ) {
+        alert("Ingresa tu nombre.");
+        return false;
+    }
+
+    if(ape == null || ape.length == 0 || /^\s+$/.test(ape) ) {
+        alert("Ingresa tu apellido");
+        return false;
+    }
+    
+    if(cor == null || cor.length == 0 || /^\s+$/.test(cor) ) {
+        alert("Debes completar las casillas!");
+        return false;
+    }
+    
+    if (contr.length <= 6 || contr.length == 0 || contr == "123456" || contr == "98754" || contr == "password" ) {
+  		alert("Contraseña no válida.");
+  		return false; 
+	}
+	/*
+	if( opcion == null || opcion == "" ) {
+	  alert("Por favor, seleccione una opción en checkbox.");
+	}
+
+	return true;    */
 }
